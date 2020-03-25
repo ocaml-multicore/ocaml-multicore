7bbc9ce70d326c6891c05b17299903b931ba6cb7
Sadiq Jaffer <sadiq@toao.com>
2020-03-25 11:54:59 +0000

    Add fast paths for caml_domain_alone

--- byterun/lockfree.c
+++ byterun/lockfree.c
@@ -17,27 +17,50 @@ void lockfree_queue_init(struct lockfree_queue* queue) {
 void lockfree_queue_push(struct lockfree_queue* queue, void* queue_item) {
     struct queue_item* new_item = (struct queue_item*)queue_item;
 
-    intptr_t current_head;
-    do {
-        current_head = atomic_load(&queue->head);
+    if( caml_domain_alone() ) 
+    {
+        new_item->next = queue->head;
+        queue->head = new_item;
+    }
+    else 
+    {
+        intptr_t current_head;
+        do {
+            current_head = atomic_load(&queue->head);
 
-        atomic_store(&new_item->next, (intptr_t)current_head);
-    } while(!atomic_compare_exchange_strong(&queue->head, (intptr_t*)&current_head, (intptr_t)new_item));
+            atomic_store(&new_item->next, (intptr_t)current_head);
+        } while(!atomic_compare_exchange_strong(&queue->head, (intptr_t*)&current_head, (intptr_t)new_item));
+    }
 }
 
 void* lockfree_queue_pop(struct lockfree_queue* queue) {
-    intptr_t current_head;
-    intptr_t new_head;
+    if( caml_domain_alone() )
+    {
+        struct queue_item* current_head = queue->head;
 
-    do {
-        current_head = atomic_load(&queue->head);
-
-        if( current_head == 0 ) {
+        if( current_head == NULL ) {
             return 0;
         }
-        
-        new_head = atomic_load(&(((struct queue_item*)current_head)->next));
-    } while(!atomic_compare_exchange_strong(&queue->head, (intptr_t*)&current_head, (intptr_t)new_head));
 
-    return (void*)current_head;
+        queue->head = current_head->next;
+
+        return current_head;
+    }
+    else
+    {
+        intptr_t current_head;
+        intptr_t new_head;
+
+        do {
+            current_head = atomic_load(&queue->head);
+
+            if( current_head == 0 ) {
+                return 0;
+            }
+            
+            new_head = atomic_load(&(((struct queue_item*)current_head)->next));
+        } while(!atomic_compare_exchange_strong(&queue->head, (intptr_t*)&current_head, (intptr_t)new_head));
+
+        return current_head;
+    }
 }
