b3aad219a11c03009fad5dad379a546f2251e9ea
Sudha Parimala <sudharg247@gmail.com>
2020-12-08 16:43:17 +0530

    caml_signal_handlers is not caml_root

--- runtime/signals.c
+++ runtime/signals.c
@@ -129,10 +129,11 @@ void caml_free_signal_stack()
 
 /* Execute a signal handler immediately */
 
-static caml_root caml_signal_handlers;
+static value caml_signal_handlers;
 
 void caml_init_signal_handling() {
-  caml_signal_handlers = caml_create_root(caml_alloc_shr(NSIG, 0));
+  caml_signal_handlers = caml_alloc_shr(NSIG, 0);
+  caml_register_generational_global_root(&caml_signal_handlers);
 }
 
 static void caml_execute_signal(int signal_number)
@@ -165,12 +166,12 @@ static void caml_execute_signal(int signal_number)
   if (caml_signal_handlers == 0) {
     res = caml_sys_exit(Val_int(2));
   } else {
-    caml_read_field(caml_read_root(caml_signal_handlers), signal_number, &handler);
+    caml_read_field(caml_signal_handlers, signal_number, &handler);
     if (!Is_block(handler)) {
       res = caml_sys_exit(Val_int(2));
     } else {
 #else
-  caml_read_field(caml_read_root(caml_signal_handlers), signal_number, &handler);
+  caml_read_field(caml_signal_handlers, signal_number, &handler);
 #endif
   res = caml_callback_exn(
            handler,
@@ -329,14 +330,14 @@ CAMLprim value caml_install_signal_handler(value signal_number, value action)
     res = Val_int(1);
     break;
   case 2:                       /* was Signal_handle */
-    caml_read_field(caml_read_root(caml_signal_handlers), sig, &handler);
+    caml_read_field(caml_signal_handlers, sig, &handler);
     res = caml_alloc_1 (0, handler);
     break;
   default:                      /* error in caml_set_signal_action */
     caml_sys_error(NO_ARG);
   }
   if (Is_block(action)) {
-    caml_modify_field(caml_read_root(caml_signal_handlers), sig, Field_imm(action, 0));
+    caml_modify_field(caml_signal_handlers, sig, Field_imm(action, 0));
   }
   caml_process_pending_signals();
   CAMLreturn (res);
