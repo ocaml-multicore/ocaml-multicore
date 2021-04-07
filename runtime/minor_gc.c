656a7cec6b6ca48ba056e8c1a48daed1f9d8d498
Enguerrand Decorne <decorne.en@gmail.com>
2021-04-07 22:37:20 +0200

    minor_gc: adding caml_global_minor_heap_start shouldnt be required here

--- runtime/minor_gc.c
+++ runtime/minor_gc.c
@@ -526,7 +526,7 @@ void caml_adjust_global_minor_heap(int participating_domains) {
   // Calculate the size of the existing mapping
   int global_minor_heap_size = caml_global_minor_heap_limit - caml_global_minor_heap_start;
   // Now using the number of participating domains, we calculate the new size
-  int new_global_minor_heap_size = participating_domains*Bsize_wsize(global_minor_heap_wsz_per_domain) + caml_global_minor_heap_start;
+  int new_global_minor_heap_size = participating_domains*Bsize_wsize(global_minor_heap_wsz_per_domain);
 
   if( global_minor_heap_size != new_global_minor_heap_size ) {
     caml_mem_decommit((char*)caml_global_minor_heap_start, global_minor_heap_size);
