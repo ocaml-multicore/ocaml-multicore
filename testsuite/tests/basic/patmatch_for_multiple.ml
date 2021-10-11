a91a60955a6f713f957d6cab68e4304ba19054de
KC Sivaramakrishnan <kc@kcsrk.info>
2021-10-11 13:25:45 +0530

    Move effect handlers to its own module in Stdlib

--- testsuite/tests/basic/patmatch_for_multiple.ml
+++ testsuite/tests/basic/patmatch_for_multiple.ml
@@ -12,16 +12,16 @@ match (3, 2, 1) with
 ;;
 [%%expect{|
 (let
-  (*match*/92 = 3
-   *match*/93 = 2
-   *match*/94 = 1
-   *match*/95 = *match*/92
+  (*match*/93 = 3
+   *match*/94 = 2
+   *match*/95 = 1
    *match*/96 = *match*/93
-   *match*/97 = *match*/94)
+   *match*/97 = *match*/94
+   *match*/98 = *match*/95)
   (catch
     (catch
-      (catch (if (!= *match*/96 3) (exit 3) (exit 1)) with (3)
-        (if (!= *match*/95 1) (exit 2) (exit 1)))
+      (catch (if (!= *match*/97 3) (exit 3) (exit 1)) with (3)
+        (if (!= *match*/96 1) (exit 2) (exit 1)))
      with (2) 0)
    with (1) 1))
 - : bool = false
@@ -36,25 +36,25 @@ match (3, 2, 1) with
 ;;
 [%%expect{|
 (let
-  (*match*/100 = 3
-   *match*/101 = 2
-   *match*/102 = 1
-   *match*/103 = (makeblock 0 *match*/100 *match*/101 *match*/102))
+  (*match*/101 = 3
+   *match*/102 = 2
+   *match*/103 = 1
+   *match*/104 = (makeblock 0 *match*/101 *match*/102 *match*/103))
   (catch
     (catch
-      (let (*match*/104 =a (field_imm 0 *match*/103))
+      (let (*match*/105 =a (field_imm 0 *match*/104))
         (catch
-          (let (*match*/105 =a (field_imm 1 *match*/103))
-            (if (!= *match*/105 3) (exit 7)
-              (let (*match*/106 =a (field_imm 2 *match*/103))
-                (exit 5 *match*/103))))
+          (let (*match*/106 =a (field_imm 1 *match*/104))
+            (if (!= *match*/106 3) (exit 7)
+              (let (*match*/107 =a (field_imm 2 *match*/104))
+                (exit 5 *match*/104))))
          with (7)
-          (if (!= *match*/104 1) (exit 6)
+          (if (!= *match*/105 1) (exit 6)
             (let
-              (*match*/108 =a (field_imm 2 *match*/103)
-               *match*/107 =a (field_imm 1 *match*/103))
-              (exit 5 *match*/103)))))
+              (*match*/109 =a (field_imm 2 *match*/104)
+               *match*/108 =a (field_imm 1 *match*/104))
+              (exit 5 *match*/104)))))
      with (6) 0)
-   with (5 x/98) (seq (ignore x/98) 1)))
+   with (5 x/99) (seq (ignore x/99) 1)))
 - : bool = false
 |}];;
