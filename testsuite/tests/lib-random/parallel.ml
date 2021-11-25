db4c557c185051c0adc0726c8e800b5f4ff026d8
Gabriel Scherer <gabriel.scherer@gmail.com>
2021-11-25 21:59:03 +0100

    testsuite/lib-random/parallel: spawn many domains
    If the number of domains is smaller than the number of cores, and each
    domain spawned runs a short computation, I couldn't easily observe
    non-deterministic scheduling effects: the domain computations would
    happen exactly in the order they are spawned, without actual
    parallelism. (In particular, a *broken* implementation of DLS
    inheritance would still pass the test.)
    
    This commit changes the test to spawn an extremely-high number of
    domains, to ensure that many of them block each other, and
    non-deterministic scheduling effects happen.

--- testsuite/tests/lib-random/parallel.ml
+++ testsuite/tests/lib-random/parallel.ml
@@ -7,8 +7,14 @@ let f () =
   let c = Random.int 100 in
   (a, b, c)
 
-let domains = List.init 10 (fun _ -> Domain.spawn f)
+(* We intentionally spawn many more domains than hardware threads, to
+   ensure that we see computations scheduled in a a non-deterministic
+   order. With a small number of domains, it's hard to reliably
+   observe non-determinism. *)
+let domain_count = 1000
+let domains = List.init domain_count (fun _i -> Domain.spawn f)
 let results = List.map Domain.join domains
+let results = List.filteri (fun i _ -> i mod (domain_count / 10) = 0) results
 
 let () =
   results |> List.iter (fun (a, b, c) ->
